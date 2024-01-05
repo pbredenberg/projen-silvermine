c8757e1bd391b7b1e23f633fb0ea1cc5ff9a4b73
PaulBredenberg <paulbredenberg@gmail.com>
2024-01-05 11:37:00 -0500

    chore: set new project defaults

--- .projenrc.ts
+++ .projenrc.ts
@@ -6,7 +6,7 @@ const project = new cdk.JsiiProject({
   authorAddress: 'paulbredenberg@gmail.com',
   defaultReleaseBranch: 'master',
   name: 'projen-silvermine',
-  repositoryUrl: 'https://github.comprojen-silvermine.git',
+  repositoryUrl: 'https://github.com/projen-silvermine.git',
   packageName: 'projen-silvermine',
   devDeps: ['fs-extra', '@types/fs-extra', 'glob'],
   peerDeps: ['projen'],
